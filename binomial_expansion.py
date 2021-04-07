9add31a4aa6e5a12dee757616c9f5a96dc49838f
OlgaARS <olyakoron@yandex.ru>
2021-04-07 21:59:26 +0300

    add test, rename file

--- binomial_expansion.py
+++ binomial_expansion.py
@@ -21,3 +21,20 @@ def expand(expr):
         res.append('1' if result == '' else result)
     res = [elem if (elem[0] == '-' or i == 0) else ('+' + elem) for i, elem in enumerate(res)]
     return ''.join(res)
+
+if __name__ == '__main__':
+    assert expand("(x+1)^0") == "1"
+    assert expand("(x+1)^1") == "x+1"
+    assert expand("(x+1)^2") == "x^2+2x+1"
+
+    assert expand("(x-1)^0") == "1"
+    assert expand("(x-1)^1") == "x-1"
+    assert expand("(x-1)^2") == "x^2-2x+1"
+
+    assert expand("(5m+3)^4") == "625m^4+1500m^3+1350m^2+540m+81"
+    assert expand("(2x-3)^3") == "8x^3-36x^2+54x-27"
+    assert expand("(7x-7)^0") == "1"
+
+    assert expand("(-5m+3)^4") == "625m^4-1500m^3+1350m^2-540m+81"
+    assert expand("(-2k-3)^3") == "-8k^3-36k^2-54k-27"
+    assert expand("(-7x-7)^0") == "1"
